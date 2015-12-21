15fd088b51bcc2a51fa405a27a30cffe2235337e
Kyle Coberly <kyle.coberly@gmail.com>
2015-12-21 11:55:19 -0700

    adding knex

---
+++ knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+        client: 'pg',
+        connection: {
+            database: 'regly',
+            user:     'regly_knex',
+            password: 'regly_password'
+        }
+    }
+};
