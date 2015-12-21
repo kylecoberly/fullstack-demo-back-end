4db92183449614c91a11de44481e5625c605d6dc
Kyle Coberly <kyle.coberly@gmail.com>
2015-12-21 11:55:53 -0700

    Switching static objects to database

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var express = require("express");
-var app = express();
+var cors = require("cors");
+var knex = require("knex")({
+    client: "pg",
+    connection: {
+        host     : "127.0.0.1",
+        user     : "regly_app",
+        password : "regly_password",
+        database : "regly"
+    }
+});
 
-var students = [{
-    id: 1,
-    first_name: "Kyle",
-    last_name: "Coberly",
-    date_of_birth: "01/01/1901",
-    email: "kyle.coberly@galvanize.com"
-},{
-    id: 2,
-    first_name: "Danny",
-    last_name: "Fritz",
-    date_of_birth: "01/01/2901",
-    email: "danny.fritz@galvanize.com"
-}];
+var app = express();
+app.use(cors());
 
 app.get("/students", function(request, response){
-    response.send({students: students});
+    knex("student").select().then(function(students){
+        response.send({students: students});
+    });
 });
 app.get("/students/:id", function(request, response){
-    response.send({students: students[request.params.id - 1]});
+    knex("student").select()
+        .where("id", request.params.id)
+        .then(function(students){
+        response.send({students: students});
+    });
 });
 
 app.listen(8000, function(){
